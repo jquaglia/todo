50a4af13e7b007b875763b25d5da148aaf636db7
jquaglia <jqugali@gmail.com>
2021-03-31 20:32:34 -0700

    got pagination working

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,32 +1,58 @@
 import React from 'react';
-import { Toast, Badge } from 'react-bootstrap';
+import { Toast, Badge, Pagination } from 'react-bootstrap';
+import { useContext, useState } from 'react';
+import { SettingsContext } from '../../context/settings/Settings.js';
 
 export default function TodoList(props) {
+
+  const context = useContext(SettingsContext);
+  const [currentPage, setCurrentPage] = useState(context.startingPage);
 
   const styles = {
     pill: { cursor: 'pointer' },
   };
 
+  const sortedList = props.list.sort((first, second) => {
+    if (second.difficulty > first.difficulty) { return 1; }
+    else if (first.difficulty > second.difficulty) { return -1; }
+    else { return 0; }
+  });
+
+  const filteredList = sortedList.filter(item => !item.complete);
+  const filteredTrueList = sortedList.filter(item => item.complete);
+  const oneListToRuleThemAll = [...filteredList, ...filteredTrueList];
+  const numberOfPages = Math.ceil(oneListToRuleThemAll.length / context.itemsPerPage);
+  
+  const indexOfLastPost = currentPage * context.itemsPerPage;
+  const indexOfFirstPost = indexOfLastPost - context.itemsPerPage;
+  const currentPosts = oneListToRuleThemAll.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  const pageNumbers = [];
+  const active = currentPage;
+  for (let number = 1; number <= numberOfPages; number++) {
+    pageNumbers.push(
+      <Pagination.Item key={number} active={number === active} href="!#" onClick={() => paginate(number)}>
+        {number}
+      </Pagination.Item>,
+    );
+  }
+
   return (
     <>
-      {props.list.map(item => (
+      {currentPosts.map(item => (
         <Toast key={item._id} onClose={() => props.handleDelete(item._id)}>
           <Toast.Header>
-
             <Badge
               pill
               style={styles.pill}
               variant={item.complete ? 'danger' : 'success'}
-              // className={`complete-${item.complete.toString()}`}
               onClick={() => props.handleComplete(item._id)}
-            // key={item._id}
             >
               {!item.complete ? 'Pending' : 'Complete'}
             </Badge>
             <strong className="mr-auto">{item.assignee}</strong>
-            {/* <span onClick={() => props.handleComplete(item._id)}> */}
-            {/* {item.text} */}
-            {/* </span> */}
           </Toast.Header>
           <Toast.Body>
             {item.text}
@@ -34,6 +60,9 @@ export default function TodoList(props) {
           </Toast.Body>
         </Toast>
       ))}
+      <Pagination>
+        {pageNumbers}
+      </Pagination>
     </>
   );
 }
--- src/hooks/useAjax.js
+++ src/hooks/useAjax.js
@@ -1,86 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// const useAjax2 = () => {
+const useAjax = () => {
 
-//   const [options, request] = useState([]);
-//   const [response, setResponse] = useState([]);
+  const [options, request] = useState([]);
+  const [response, setResponse] = useState([]);
 
-//   useEffect(() => {
-//     async function ajax() {
-//       const res = await axios(options);
-//       setResponse(res.data);
-//     }
-//     ajax();
-//   }, [options]);
-
-//   // request is our setter for updating request details
-//   // respoknse is our getter for the axios results
-//   return [response, request];
-// };
-
-
-const useAjax = (url) => {
-
-  const [list, setList] = useState([]);
-
-  const _axiosAddItem = async item => {
-    const request = await axios({
-      method: 'post',
-      url: url,
-      data: item,
-    });
-
-    // setList([...list, request.data]);
-    _axiosGetItems();
-    return request;
-  };
-
-  const _axiosGetItems = async item => {
-    try {
-      const request = await axios({
-        method: 'get',
-        url: url,
-      });
-      // console.log(request);
-      const results = await request.data.results;
-      // console.log(results);
-      // console.log('B', list);
-      setList([...list, results]);
-      // console.log('T', list);
+  useEffect(() => {
+    async function ajax() {
+      if (!options.url) return;
+      const res = await axios(options);
+      setResponse(res.data);
     }
-    catch (e) {
-      console.log(e);
-    }
-  };
+    ajax();
+  }, [options]);
 
-  // useEffect(_axiosGetItems, []);
-
-  const _toggleComplete = async id => {
-    const item = list.filter(i => i._id === id)[0] || {};
-
-    if (item._id) {
-      const url = `${url}/${id}`;
-      const request = await axios({
-        method: 'put',
-        url: url,
-        data: {
-          complete: !item.complete,
-        },
-      });
-
-      // setList(list.map(listItem => listItem._id === item._id ? request : listItem));
-      _axiosGetItems();
-      return request;
-    }
-  };
-
-  return [
-    list,
-    _axiosAddItem,
-    _axiosGetItems,
-    _toggleComplete,
-  ];
+  return [response, request];
 };
 
 export default useAjax;
